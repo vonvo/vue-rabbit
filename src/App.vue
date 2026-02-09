e2a9de137d8d66105a9d9eb4c1938a0a78125f5e
chengxin <19856638896@163.com>
2026-02-09 22:50:55 +0800

    把layout的子组件错误地写在app组件上

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <script setup>
-import LayoutNav from '@/views/Layout/components/LayoutNav.vue'
-import LayoutHeader from '@/views/Layout/components/LayoutHeader.vue'
-import LayoutFooter from '@/views/Layout/components/LayoutFooter.vue'
+
 
 </script>
 
 <template>
-   <LayoutNav />
-   <LayoutHeader />
+   
    <!-- //这是一级路由出口组件,在这里渲染 -->
    <RouterView/>
-   <el-button type="primary">主要按钮</el-button>
-   <LayoutFooter />
+   <!-- <el-button type="primary">主要按钮</el-button> -->
+   
 </template>
 
 <style scoped></style>
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,7 +1,17 @@
+<script setup>
+import LayoutNav from '@/views/Layout/components/LayoutNav.vue'
+import LayoutHeader from '@/views/Layout/components/LayoutHeader.vue'
+import LayoutFooter from '@/views/Layout/components/LayoutFooter.vue'
+
+</script>
+
 <template>
     <div>
+        <LayoutNav />
+        <LayoutHeader />
         我是首页
         <!-- 这是二级路由出口组件 -->
         <RouterView/>
+        <LayoutFooter />
     </div>
 </template>

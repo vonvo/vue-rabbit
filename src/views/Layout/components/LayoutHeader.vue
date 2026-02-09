7542d670428c36a49bb2b19b77cad99c7be55606
chengxin12 <chengxin12@qq.com>
2026-02-09 16:59:54 +0800

    渲染一级导航列表

--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -1,4 +1,22 @@
 <script setup>
+import {getCategoryAPI} from  "@/apis/Layout"
+import { onMounted, ref } from "vue";
+
+const categoryList=ref([])
+
+const getCategory=async ()=>{
+    const res=await getCategoryAPI()
+    console.log(res);
+    categoryList.value=res.result
+
+    
+}
+
+onMounted(()=>{
+    getCategory()
+})
+
+
 
 </script>
 
@@ -12,9 +30,9 @@
         <li class="home">
           <RouterLink to="/">首页</RouterLink>
         </li>
-        <li> <RouterLink to="/">居家</RouterLink> </li>
-        <li> <RouterLink to="/">美食</RouterLink> </li>
-        <li> <RouterLink to="/">服饰</RouterLink> </li>
+        <li v-for="items in categoryList" :key="items.id"> 
+            <RouterLink to="/" >{{ items.name }}</RouterLink> 
+        </li>
       </ul>
       <div class="search">
         <i class="iconfont icon-search"></i>

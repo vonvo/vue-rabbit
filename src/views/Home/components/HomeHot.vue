43697f75c658c0faac5a37e65045553f325b3b1a
chengxin12 <chengxin12@qq.com>
2026-02-10 16:32:52 +0800

    home-人气推荐业务实现

--- src/views/Home/components/HomeHot.vue
+++ src/views/Home/components/HomeHot.vue
@@ -1,5 +1,79 @@
+<script setup>
+import { getHotAPI } from '@/apis/Home';
+import { onMounted, ref } from 'vue';
+import HomePanel from '@/views/Home/components/HomePanel.vue'
+const hotList=ref([])
+const getfindHot=async()=>{
+    const res=await getHotAPI()
+    console.log(res);
+    hotList.value=res.result
+}
+
+onMounted(()=>{
+    getfindHot()
+})
+
+
+</script>
+
 <template>
-    <div>
-        我是人气推荐
-    </div>
+    <HomePanel title="人气推荐" subTitle="人气爆款 不容错过">
+        <ul class="goods-list">
+            <li v-for="item in hotList" :key="item.id">
+            <RouterLink to="/">
+                <img :src="item.picture" alt="" />
+                <p class="name">{{ item.title }}</p>
+                <p class="desc">&yen;{{ item.alt }}</p>
+            </RouterLink>
+            </li>
+        </ul>
+    </HomePanel>
+
+  <!-- 下面是插槽主体内容模版
+  <ul class="goods-list">
+    <li v-for="item in newList" :key="item.id">
+      <RouterLink to="/">
+        <img :src="item.picture" alt="" />
+        <p class="name">{{ item.name }}</p>
+        <p class="price">&yen;{{ item.price }}</p>
+      </RouterLink>
+    </li>
+  </ul>
+  -->
 </template>
+
+
+<style scoped lang='scss'>
+.goods-list {
+  display: flex;
+  justify-content: space-between;
+  height: 426px;
+
+  li {
+    width: 306px;
+    height: 406px;
+    transition: all .5s;
+
+    &:hover {
+      transform: translate3d(0, -3px, 0);
+      box-shadow: 0 3px 8px rgb(0 0 0 / 20%);
+    }
+
+    img {
+      width: 306px;
+      height: 306px;
+    }
+
+    p {
+      font-size: 22px;
+      padding-top: 12px;
+      text-align: center;
+    }
+
+    .desc {
+      color: #999;
+      font-size: 18px;
+    }
+  }
+}
+</style>

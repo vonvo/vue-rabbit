1c0a1c54db25048bebbb865bf343ca9c56427fbd
chengxin12 <chengxin12@qq.com>
2026-02-10 12:50:44 +0800

    banner轮播图实现

---
+++ src/apis/home.js
@@ -0,0 +1,10 @@
+//根据接口文档封装接口函数，发送请求获取数据列表，v-for渲染页面
+//封装获取banner的接口函数
+
+import httpInstance from "@/utils/http";
+
+export function getBannerAPI(){
+    return httpInstance({   
+        url:"/home/banner"
+    })
+}

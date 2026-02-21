54bfc8f787f0af600c7987bb4efec1ddf554c483
chengxin <19856638896@163.com>
2026-02-22 01:26:41 +0800

    登录和非登录状态下的模板适配

--- src/views/Layout/components/LayoutNav.vue
+++ src/views/Layout/components/LayoutNav.vue
@@ -1,5 +1,6 @@
 <script setup>
-
+import {useUserStore} from "@/stores/user"
+const userStore=useUserStore()
 </script>
 
 <template>
@@ -7,8 +8,9 @@
     <div class="container">
       <ul>
         <!-- 多模板渲染，区分登陆状态和非登陆状态 -->
-        <template v-if="false">
-          <li><a href="javascript:;"><i class="iconfont icon-user"></i>周杰伦</a></li>
+         <!-- 多模版适配的通用思路:有几个需要适配的模版就准备几个template片段，通过条件渲染控制显示即可 -->
+        <template v-if="userStore.userInfo?.token">
+          <li><a href="javascript:;"><i class="iconfont icon-user"></i>{{ userStore.userInfo?.account }}</a></li>
           <li>
             <el-popconfirm title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
               <template #reference>
